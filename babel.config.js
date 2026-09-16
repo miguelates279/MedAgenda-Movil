5da0c308f0361b7df5b5c53578f373b58416e65e
mso2024 <miguel.suarezo@autonoma.edu.co>
2026-09-16 18:08:03 -0500

    fix: transform private properties and class properties in babel config for Hermes

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+          unstable_transformProfile: "hermes-v0",
+        },
+      ],
       "nativewind/babel",
+    ],
+    plugins: [
+      "@babel/plugin-transform-class-properties",
+      "@babel/plugin-transform-private-methods",
+      "@babel/plugin-transform-private-property-in-object",
+      "react-native-reanimated/plugin",
     ],
   };
 };
