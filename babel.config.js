59d7ae7fad49f8258f9eda54998f9815e400453f
mso2024 <miguel.suarezo@autonoma.edu.co>
2026-09-16 18:23:59 -0500

    fix: use unstable_transformProfile hermes-v0 in babel-preset-expo for complete class transform

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+          unstable_transformProfile: "hermes-v0",
+        },
+      ],
       "nativewind/babel",
     ],
     plugins: [
-      ["@babel/plugin-transform-class-properties", { loose: true }],
-      ["@babel/plugin-transform-private-methods", { loose: true }],
-      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
       "react-native-reanimated/plugin",
     ],
   };
