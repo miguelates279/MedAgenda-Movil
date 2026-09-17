471aec1e854645d079bd2631981ad845fe96ce3d
mso2024 <miguel.suarezo@autonoma.edu.co>
2026-09-17 12:56:16 -0500

    chore: clean up duplicate api methods and sync merge with Main

--- src/api/clinics.ts
+++ src/api/clinics.ts
@@ -98,10 +98,6 @@ export const clinicsApi = {
       `/clinics/getClinicDoctorAppointmentsForDay?clinic_id=${clinicId}&doctor_id=${doctorId}&appointment_date=${appointmentDate}`
     );
   },
-
-  async getUserClinics(): Promise<Clinic[]> {
-    return apiClient.get<Clinic[]>('/clinics/userClinics');
-  },
 };
 
 export default clinicsApi;
